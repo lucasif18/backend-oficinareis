c8893f0f701d0b11f263937c43eab1d452a957cd
lucasif18 <20182tadssaj0018@ifba.edu.br>
2025-10-26 23:24:20 -0300

    Criação de dois arquivos front

---
+++ frontend/src/main/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Oficina Reis - Sistema de Gestão</h1>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 50px;
+  color: #2c3e50;
+}
+</style>
